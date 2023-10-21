2ef277496ff208d2b464c6196c2ec77739511316
zehowrld <hiteshnegi08@gmail.com>
2023-10-22 02:49:39 +0530

    web app

--- application.py
+++ application.py
@@ -9,13 +9,15 @@ application = Flask(__name__)
 app=application
 # Route for a home page
 @app.route('/')
+@cross_origin()
 def index():
     return render_template('index.html')
 
 @app.route('/predictdata', methods=['GET', 'POST'])
+@cross_origin()
 def predict_datapoint():
     if request.method == 'GET':
-        return render_template('home.html')
+        return render_template('index.html')
     else:
         data = CustomData(
             gender=request.form.get('gender'),
@@ -31,7 +33,7 @@ def predict_datapoint():
 
         predict_pipeline = PredictPipeline()
         results = predict_pipeline.predict(pred_df)
-        return render_template('home.html', results=results[0])
+        return render_template('index.html', results=results[0])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
